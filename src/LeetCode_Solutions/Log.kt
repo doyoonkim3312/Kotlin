6e15ce20ce32b90c9d7e6294585fc700fa5d5a38
Doyoon Kim <61890844+doyoonkim3312@users.noreply.github.com>
2024-06-02 14:32:18 +0000

    Q92 ReverseLinkedList
    * Initial Approach completed. Should be revised.
    
    * Log.kt
      Kotlin class that mimics Log class in Android Library.

---
+++ src/LeetCode_Solutions/Log.kt
@@ -0,0 +1,10 @@
+package LeetCode_Solutions
+
+class Log {
+    companion object {
+        // debug
+        fun d(tag: String = "Debug", msg: String) {
+            println("[$tag]\t$msg")
+        }
+    }
+}
--- src/LeetCode_Solutions/Q92_ReverseLinkedList_Two.kt
+++ src/LeetCode_Solutions/Q92_ReverseLinkedList_Two.kt
@@ -13,8 +13,19 @@ fun main() {
     testSet.next = ListNode(2)
     testSet.next?.next = ListNode(3)
     testSet.next?.next?.next = ListNode(4)
+    testSet.next?.next?.next?.next = ListNode(5)
+    testSet.next?.next?.next?.next?.next = ListNode(6)
+    testSet.next?.next?.next?.next?.next?.next = ListNode(7)
 
+    /*
     var result = reverseLinkedList(testSet, null)
+    while (result != null) {
+        print("${result.`val`} ")
+        result = result.next
+    }
+     */
+
+    var result = q92InitialSolution(testSet, 2, 3)
     while (result != null) {
         print("${result.`val`} ")
         result = result.next
@@ -23,26 +34,42 @@ fun main() {
 }
 
 fun q92InitialSolution(head: ListNode?, left: Int, right: Int): ListNode? {
+
     var h = head
     var tracking = head
 
-    var targetTracking = ListNode(0)
-    val targetHead = targetTracking
+    var targetTracking: ListNode
+    val targetHead = ListNode(0).also {
+        targetTracking = it
+    }
 
     var st: ListNode? = null
-
+    var prev: ListNode? = null
     var cnt = 1
 
-    while (tracking?.next != null) {
-        if (cnt == left - 1) st = tracking
+    while (tracking != null) {
+        //if (cnt == left - 1) st = prev
+        if (cnt == right + 1) {
+            // call reverseLinkedList
+            targetTracking.next = null
+            val reversed = reverseLinkedList(targetHead.next, null)
+            targetHead.next?.next = tracking
+
+
+            prev?.next = reversed
+            return h
+        }
         if (cnt in left..right) {
+            Log.d(msg = "Current Tracking :${tracking.`val`}")
             targetTracking.next = tracking
 
             targetTracking = targetTracking.next!!
-            tracking = tracking.next
         } else {
-            tracking = tracking.next
+            prev = tracking
         }
+
+        tracking = tracking.next
+        cnt++
     }
 
     return null
@@ -51,6 +78,7 @@ fun q92InitialSolution(head: ListNode?, left: Int, right: Int): ListNode? {
 
 
 fun reverseLinkedList(head: ListNode?, prev: ListNode?): ListNode? {
+    Log.d(msg = "reverseLinkedList has been called; head: ${head?.`val`} prev: ${prev?.`val`}")
     if (head == null) return prev
 
     val oldNext = head.next
