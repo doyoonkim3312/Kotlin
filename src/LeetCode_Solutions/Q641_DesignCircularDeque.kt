4caff41c80644840a8404dd6ee9ec576465086bf
Doyoon Kim <61890844+doyoonkim3312@users.noreply.github.com>
2024-07-11 22:40:35 +0900

    Update Q641_DesignCircularDeque.kt
    * Improved solution has been applied & validated using test cases provided in the question.
      - It passes the test case.

--- src/LeetCode_Solutions/Q641_DesignCircularDeque.kt
+++ src/LeetCode_Solutions/Q641_DesignCircularDeque.kt
@@ -4,7 +4,8 @@ package LeetCode_Solutions
  * LeetCode 641: Design Circular Deque.
  */
 fun main() {
-    val obj = MyCircularDeque(3)
+//     val obj = MyCircularDeque(3)
+    val obj = EnhancedCircularDeque(3)
     
     obj.insertLast(1).also { println(it) }
     obj.insertLast(2).also { println(it) }
@@ -122,4 +123,89 @@ class MyCircularDeque(k: Int) {
         return cnt == size
     }
 
+}
+
+
+// Improved Solution
+// "LinkedList is a data-type where an empty space does not exists."
+class EnhancedCircularDeque(k: Int) {
+    // Nested class defined as a static-nested class by default.package
+    class EnhancedNode(val item: Int, var left: EnhancedNode? = null, var right: EnhancedNode? = null)
+    
+    private var head: EnhancedNode = EnhancedNode(-1)		// Dummy Node.
+    private var tail: EnhancedNode = EnhancedNode(-1)		// Dummy Node.
+    private var len: Int = 0
+    private var size = k
+    
+    init {
+        // Make a linkage between head and tail. Head & tail would play a role of start/end points.
+        head.left = tail.also { it.right = head }
+        head.right = tail.also { it.left = head }
+    }
+    
+    fun insertFront(value: Int): Boolean {
+        if (len == size) return false		// Early-return
+        else {
+            val temp = EnhancedNode(value)
+            
+            temp.right = head.right
+            head.right?.left = temp
+            temp.left = head
+            head.right = temp
+            
+            len++
+            return true
+        }
+    }
+    
+    fun insertLast(value: Int): Boolean {
+        if (len == size) return false		// Early-return
+        else {
+            val temp = EnhancedNode(value)
+            
+            temp.left = tail.left
+            tail.left?.right = temp
+            temp.right = tail.also { it.left = temp }
+            
+            len++
+            return true
+        }
+    }
+    
+    fun deleteFront(): Boolean {
+        if (len == 0) return false		// Early-return
+        else {
+            head.right = head.right?.right?.also { it.left = head }
+            len--
+            
+            return true
+        }
+    }
+    
+    fun deleteLast(): Boolean {
+        if (len == 0) return false		// Early-return
+        else {
+            tail.left = tail.left?.left?.also { it.right = tail }
+            len--
+            
+            return true
+        }
+    }
+    
+    fun getFront(): Int {
+        return head.right?.item ?: -1
+    }
+    
+    fun getRear(): Int {
+        return tail.left?.item ?: -1
+    }
+    
+    fun isEmpty(): Boolean {
+        return len == 0
+    }
+    
+    fun isFull(): Boolean {
+        return len == size
+    }
+    
 }
